148d3ed1ddebd059f52ccc561f6c1e105a506610
Mr-Robinhood <donomer98@gmail.com>
2024-01-30 17:55:43 +0200

    task 0

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 	return (-1);
 	va_start(list_of_args, format);
-	while (*format++ != '\0')
+	while (*format)
 	{
 		if (*format != '%')
 		{	write(1, format, 1);
